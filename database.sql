b188e1829aa298f6deb54f10e0ba4f7517101bba
ali-hbl <nwah.ally@gmail.com>
2025-09-18 16:10:56 +0200

    feature: config php and database

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE notebox;
+
+USE notebox;
+
+CREATE TABLE notes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
